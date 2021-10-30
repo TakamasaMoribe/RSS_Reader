1fed199bdfcbe16e29600ebcff687020fef22978
森部高昌 <takamasamoribe@gmail.com>
2021-10-30 22:37:13 +0900

    1030  4   addressと　緯度経度の取得ができそうである

--- RSS_Reader/ViewController.swift
+++ RSS_Reader/ViewController.swift
@@ -13,24 +13,14 @@
 
         @IBOutlet weak var tableView: UITableView!
 
-//        let feedUrl = URL(string:"https://news.yahoo.co.jp/rss/topics/top-picks.xml")!
         let feedUrl = URL(string:"https://geocode.csis.u-tokyo.ac.jp/cgi-bin/simple_geocode.cgi?addr=%E6%96%B0%E5%AE%BF")! //新宿
-        var feedItems = [FeedItem]()
+        
+        var feedItems = [FeedItem]() // tableViewの表示に使っている
         
         var currentElementName:String! // パース中に、読み出している項目名
         
         //var searchList:[(address:String,longitude:String,latitude:String)] = [] // タプルに入れてみる
         
-        //let ITEM_ELEMENT_NAME = "address"
-        
-
-        var flagAddress : Bool = false
-        var flagLongitude : Bool = false
-        var flagLatitude : Bool = false
-
-        // １階層目のタグを覚えておく変数を用意する
-        var XMLtag1:String! = ""
-        var XMLtag2:String! = "" // ２階層目
         
 // ----------------------------------------------------------------------------------
         override func viewDidLoad() {
@@ -48,30 +38,6 @@
         
 // ----------------------------------------------------------------------------------
         
-//        //タグの最初が見つかったとき呼ばれる
-//        func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-//            //self.currentElementName = nil // 初期化
-//
-//            // タグが results ならば 「resultsタグ」に入ったことを、XMLtag1に覚えておく
-//            if elementName == "results" { XMLtag1 = "results" }
-//            // タグが candidate ならば 「candidateタグ」に入ったことを、XMLtag2に覚えておく
-//            if elementName == "candidate" { XMLtag2 = "candidate" }
-//            // タグが address ならば
-//            if elementName == "address" { flagAddress = true
-//
-//            }
-//            // タグが longitude ならば
-//            if elementName == "longitude" { flagLongitude = true }
-//            // タグが latitude ならば
-//            if elementName == "latitude" { flagLatitude = true }
-//
-//            // タグ1が「results」で、タグ2が「candidate」に入った状態ならば、そのデータを取り出す
-////            if XMLtag1 == "results" && XMLtag2 == "candidate" {
-////
-////                //print("elementName:\(elementName)")//
-////            }
-//        }
-        
         //タグの最初が見つかったとき呼ばれる
         func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
             self.currentElementName = nil // 初期化
@@ -83,66 +49,45 @@
             }
         }
         
-        
-            
         // 開始タグと終了タグでくくられたデータがあったときに実行されるメソッド。stringが得られる
          func parser(_ parser: XMLParser, foundCharacters string: String) {
              
-             if self.feedItems.count > 0 {
-                 let lastItem = self.feedItems[self.feedItems.count - 1]
+             if string != "\n" {// 改行でなければ読み取る
                  
-                 switch self.currentElementName {
-                 case "address":
-                     let tmpString = lastItem.address
-                     lastItem.address = (tmpString != nil) ? tmpString! + string : string
-                     if string != "\n" {print("address:\(string)")}//後ろ側の"address"を読んだときに、""になる
+                 if self.feedItems.count > 0 {
+                     let lastItem = self.feedItems[self.feedItems.count - 1]
                      
-                 case "longitude":
-                     let tmpString = lastItem.longitude
-                     lastItem.longitude = (tmpString != nil) ? tmpString! + string : string
-                     if string != "\n" {print("longitude:\(string)")}//後ろ側の"longitude"を読んだときに、""になる
-                     
-                 case "latitude":
-                     let tmpString = lastItem.latitude
-                     lastItem.latitude = (tmpString != nil) ? tmpString! + string : string
-                     if string != "\n" {print("latitude:\(string)")}//後ろ側の"latitude"を読んだときに、""になる
-                     
-                 default:
-                     break
-                 }
-                 
+                    switch self.currentElementName {
+                     case "address":
+                         let tmpString = lastItem.address
+                         lastItem.address = (tmpString != nil) ? tmpString! + string : string
+                         print("address:\(string)")//
+                     case "longitude":
+                         let tmpString = lastItem.longitude
+                         lastItem.longitude = (tmpString != nil) ? tmpString! + string : string
+                         print("longitude:\(string)")//
+                     case "latitude":
+                         let tmpString = lastItem.latitude
+                         lastItem.latitude = (tmpString != nil) ? tmpString! + string : string
+                         print("latitude:\(string)")//
+                     default:
+                         break
+                    }
+                }
              }
-             
-             
-//             if (flagAddress == true)  { print("Address:\(string)")
-//                 address = string
-//                 self.feedItems.append(FeedItem()) // tableViewのデータに対応している？
-//                 print(feedItems)
-//             }
-//
-//             if (flagLongitude == true)  { print("Longitude:\(string)")
-//                 longitude = string
-//             }
-//             if (flagLatitude == true)  { print("Latitude:\(string)")
-//                 latitude = string
-//             }
-             
-             //let item = (address,longitude,latitude)
-             //print("item:\(item)")
-             
-             flagAddress = false
-             flagLongitude = false
-             flagLatitude = false
          }
         
         // タグの終わりが見つかったとき呼ばれる
         func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
 
-            // タグが"results"ならば、タグが閉じられたので、XMLtag1,2をリセットする
+            // タグが"results"ならば、タグが閉じられた
             if elementName == "results" {
                 print("おわり:")//確認ok
-             XMLtag1 = ""
-             XMLtag2 = ""
+                
+                let feedItem = self.feedItems
+                print(feedItem[1].address)//optionalになっている
+                print(feedItem[1].longitude)//optionalになっている
+                print(feedItem[1].latitude)//optionalになっている
             }
         }
  
@@ -168,7 +113,8 @@
         }
     }
 
-// 変更する？？？
+
+//
     class FeedItem {
         var address: String!
         var longitude: String!
